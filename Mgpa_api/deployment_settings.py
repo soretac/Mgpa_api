86bc148571ade8d4e76687e636002d9f601394cd
soretac <duparce.netzone@gmail.com>
2026-09-17 11:39:05 +0100

    update deployment settings 4

--- Mgpa_api/deployment_settings.py
+++ Mgpa_api/deployment_settings.py
@@ -216,9 +216,9 @@ DATE_INPUT_FORMATS = ['%d-%m-%Y'] # Used in form submissions
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'build/static')
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
