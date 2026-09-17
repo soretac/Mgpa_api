e63d40caf11c7ab2f2e931eb1addc435d98318e7
soretac <duparce.netzone@gmail.com>
2026-09-17 14:41:59 +0100

    update deployment settings 14

--- build.sh
+++ build.sh
@@ -4,17 +4,19 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+python manage.py migrate_schemas
+
 # python manage.py migrate_schemas --shared 
 
 # python manage.py migrate_schemas --tenant 
 
-python manage.py migrate Client
-python manage.py migrate CompteurApps
-python manage.py migrate Corrective
-python manage.py migrate Entreprise
-python manage.py migrate MatRoulant
-python manage.py migrate Parameters
-python manage.py migrate PreventApps
-python manage.py migrate Travaux
-python manage.py migrate MgpaUsers
+# python manage.py migrate Client
+# python manage.py migrate CompteurApps
+# python manage.py migrate Corrective
+# python manage.py migrate Entreprise
+# python manage.py migrate MatRoulant
+# python manage.py migrate Parameters
+# python manage.py migrate PreventApps
+# python manage.py migrate Travaux
+
 
