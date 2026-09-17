3df52fc5bd59c42f48d3e1b50bfc573a399de129
soretac <duparce.netzone@gmail.com>
2026-09-17 15:37:44 +0100

    update deployment settings 19

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+python manage.py makemigrations
+
 # python manage.py migrate_schemas
 
 # python manage.py migrate_schemas --shared 
